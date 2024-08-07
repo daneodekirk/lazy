8d0007dd404e73547ce42b8db07114cde7f63158
Dane Odekirk <daneodekirk@gmail.com>
2024-08-07 19:19:40 +0200

    Adding gitsigns visual cue

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,14 +1,13 @@
 return {
   'lewis6991/gitsigns.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   opts = {
-    signcolumn = auto,
     current_line_blame = true,
-    on_attach = function()
-      vim.wosigncolumn = "yes"
-    end
   },
   keys = {
     { "<leader>gp", "<cmd>:Gitsigns preview_hunk_inline<cr>", desc = "Preview hunk inline", nowait = false, remap = false },
     { "<leader>gr", "<cmd>:Gitsigns reset_hunk<cr>", desc = "Reset hunk", nowait = false, remap = false },
+    { "<leader>gn", "<cmd>:Gitsigns next_hunk<cr>", desc = "Next hunk", nowait = false, remap = false },
+    { "<leader>gb", "<cmd>:Gitsigns prev_hunk<cr>", desc = "Prev hunk", nowait = false, remap = false },
   }
 }
