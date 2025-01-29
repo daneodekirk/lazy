460e51f1943e443cfd3caad73436d40c811ab943
Dane Odekirk <daneodekirk@gmail.com>
2025-01-29 20:09:52 +0100

    Adding Aerial

---
+++ lua/plugins/navigation/aerial.lua
@@ -0,0 +1,13 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  },
+  keys = {
+    { "<leader>A", desc="Aerial" },
+    { "<leader>AA", ':AerialToggle <cr>', desc="Aerial Toggle"},
+  }
+}
