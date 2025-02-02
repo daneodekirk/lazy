40686c78685562dec2051fc4a1b845212c23efb5
Dane Odekirk <daneodekirk@gmail.com>
2025-02-02 10:00:46 +0100

    Removing commented code

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -10,23 +10,12 @@ vim.opt.softtabstop = 2
 
 vim.o.encoding = 'utf-8'
 
---
--- keymaps
---
---
 vim.o.autochdir = false
 
 -- leader
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ";"
 
--- terminal visual mode
--- vim.keymap.set('t', '<leader><Esc>', '<C-\\><C-n>', { noremap=true, silent=true, desc = 'Toggle terminal mode'})
--- vim.keymap.del('n', 'q')
-
 --- remap recording
 vim.keymap.set("n", "q", "<Nop>", {silent=true, noremap=true})
 vim.keymap.set('n', '<leader>q', 'q', { noremap=true, silent=true, desc = 'Record'})
-
--- vim.opt.hlsearch = true
--- vim.opt.incsearch = true
