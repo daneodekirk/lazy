96fe488ff65c2c80a2eb4e22f0553572a881148b
Dane Odekirk <daneodekirk@gmail.com>
2025-08-17 16:27:43 +0200

    Adding keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,19 +1,17 @@
 return {
-	"folke/which-key.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	keys = {
-		-- lazy
-		{ "<leader>l", "<cmd>:Lazy <cr>", desc = "Open Lazy" },
+  "folke/which-key.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    -- lazy
+    { "<leader>l",        "<cmd>:Lazy <cr>",         desc = "Open Lazy" },
 
-		-- navigation
-		{ "<leader>b", group = "Jump Navigation", nowait = false, remap = false },
-		{ "<leader>bb", "<c-o>", desc = "Jump back", nowait = false, remap = false },
-		-- { "<leader>bd", "<cmd>:bd <cr>", desc = "Delete buffer", nowait = false, remap = false },
-		{ "<leader>bn", "<c-i>", desc = "Jump forward", nowait = false, remap = false },
-
-		-- buffers
-		{ "<leader>q", ":close<cr>", desc = "Hide buffer", mode = "n", nowait = false, remap = false },
-	},
+    { "<leader>b",        group = "Jump Navigation", nowait = false,        remap = false },
+    { "<leader>bb",       "<c-o>",                   desc = "Jump back",    nowait = false, remap = false },
+    { "<leader>bn",       "<c-i>",                   desc = "Jump forward", nowait = false, remap = false },
+    { "<leader>q",        ":close<cr>",              desc = "Hide buffer",  mode = "n",     nowait = false, remap = false },
+    { "<leader><leader>", ":w<cr>",                  desc = "Write buffer", mode = "n",     nowait = false, remap = false },
+    { "<leader>ww",       "<c-w>=",                  desc = "Reset Window", mode = "n",     nowait = false, remap = false },
+  },
 }
